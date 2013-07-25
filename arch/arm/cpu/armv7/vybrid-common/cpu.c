e46247294a6cdbca2ef23d88edb34a09f032b472
Hartmut Keller <keller@fs-net.de>
2013-07-25 14:21:51 +0200

    Re-implement unbearable output of reset cause
    The function to show the reset cause was implemented remarkably bad.
    This is improved now. See arch/arm/cpu/armv7/vybrid-common/cpu.c.

--- arch/arm/cpu/armv7/vybrid-common/cpu.c
+++ arch/arm/cpu/armv7/vybrid-common/cpu.c
@@ -35,62 +35,67 @@
 #include <fsl_esdhc.h>
 #endif
 
-static char *get_reset_cause(void)
+static void print_reset_cause(void)
 {
-	char resetcause[32][64] = {"POR",
-				   "Cortex A5 WDOG Timer Reset",
-				   0,
-				   "CA5 WDOG reset",
-				   "CM4 WDOG reset",
-				   "JTAG HIGH-Z",
-				   0,
-				   "External Reset",
-				   "1.2V supply below 0.7V",
-				   "HP regulator's LVD",
-				   "ULP regulator's LVD",
-				   "3.3V main supply is unstable",
-				   "LP regulator's LVD",
-				   0,
-				   0,
-				   0,
-				   "MDM-AP system reset request is set",
-				   "Hard Fail State of System Security Monitor",
-				   "SRC_SCR SW Reset is set",
-				   "Platform's CSU alarm event",
-				   0,
-				   0,
-				   0,
-				   0,
-				   "Anadig regulator 1.1V unstable",
-				   "Anadig regulator 2.5V unstable",
-				   "Anadig regulator 3.0V unstable",
-				   "CMU even when FOSC freq less than 40MHz",
-				   "CMU event when BUS freq is out of range",
-				   "No clock is detected on FOSC",
-				   "No clock is detected on SOSC",
-				   "CM4 is in lockup"};
-	char buf[512] = {0}, *pbuf;
+	const char *const resetcause[32] = {
+		"Power-on Reset",
+		"Cortex A5 WDOG Timer Reset",
+		NULL,
+		"CA5 WDOG reset",
+		"CM4 WDOG reset",
+		"JTAG HIGH-Z",
+		NULL,
+		"External Reset",
+		"1.2V supply below 0.7V",
+		"HP regulator's LVD",
+		"ULP regulator's LVD",
+		"3.3V main supply is unstable",
+		"LP regulator's LVD",
+		NULL,
+		NULL,
+		NULL,
+		"MDM-AP system reset request is set",
+		"Hard Fail State of System Security Monitor",
+		"SRC_SCR SW Reset is set",
+		"Platform's CSU alarm event",
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		"Anadig regulator 1.1V unstable",
+		"Anadig regulator 2.5V unstable",
+		"Anadig regulator 3.0V unstable",
+		"CMU even when FOSC freq less than 40MHz",
+		"CMU event when BUS freq is out of range",
+		"No clock is detected on FOSC",
+		"No clock is detected on SOSC",
+		"CM4 is in lockup"
+	};
 	u32 cause;
-	int i, bit;
+	int i;
+	int count = 0;
 	struct src *src_regs = (struct src *)SRC_BASE_ADDR;
 
 	cause = readl(&src_regs->srsr);
 	writel(cause, &src_regs->srsr);
 
-	pbuf = &buf[0];
-
 	for (i = 0; i < 32; i++) {
-		bit = 1 << i;
-		if ((cause & bit) == bit) {
-			if (resetcause[i][0] != NULL)
-				pbuf += sprintf(pbuf, "%s, ", resetcause[i]);
+		if ((cause & (1 << i)) && resetcause[i]) {
+			if (count)
+				puts(", ");
+			else
+				puts("Reset: ");
+			puts(resetcause[i]);
+			count++;
+
+			/* If it is a power-on reset, several bits are set;
+			   just report the first, i.e. ower-on reset itself */
+			if (i == 0)
+				break;
 		}
 	}
-
-	if (buf[0] == NULL)
-		return 0;
-
-	return &buf[0];
+	if (count)
+		putc('\n');
 }
 
 #if defined(CONFIG_DISPLAY_CPUINFO)
@@ -104,8 +109,8 @@ int print_cpuinfo(void)
 		(cpurev & 0x000F0) >> 4,
 		(cpurev & 0x0000F) >> 0,
 		vybrid_get_clock(VYBRID_ARM_CLK) / 1000000);
-	if (get_reset_cause() != NULL)
-		printf("Reset cause: %s\n", get_reset_cause());
+	print_reset_cause();
+
 	return 0;
 }
 #endif
