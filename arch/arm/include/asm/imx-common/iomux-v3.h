8390a9803dad2254897a8541f208b60882a33fc8
Hartmut Keller <keller@fs-net.de>
2015-08-03 10:23:01 +0200

    Add IOMUX_PADS() and SETUP_IOMUX_PADS() for i.MX6 Solo-X
    To be able to stay as similar as possible to the code for the regular
    i.MX6, we want to have macros IOMUX_PADS(), SETUP_IOMUX_PAD() and
    SETUP_IOMUX_PADS(), even if there are no different settings required
    at the moment (like on i.MX6 Quad/Dual and DualLite/Solo). This may
    prove helpful in the future, too.

--- arch/arm/include/asm/imx-common/iomux-v3.h
+++ arch/arm/include/asm/imx-common/iomux-v3.h
@@ -194,10 +194,16 @@ if (is_cpu_type(MXC_CPU_MX6Q) || is_cpu_type(MXC_CPU_MX6D)) {	\
 	imx_iomux_v3_setup_pad(MX6Q_##def);
 #define SETUP_IOMUX_PADS(x)					\
 	imx_iomux_v3_setup_multiple_pads(x, ARRAY_SIZE(x))
-#else
+#elif defined(CONFIG_MX6DL) || defined(CONFIG_MX6S)
 #define IOMUX_PADS(x) MX6DL_##x
 #define SETUP_IOMUX_PAD(def)					\
 	imx_iomux_v3_setup_pad(MX6DL_##def);
+#define SETUP_IOMUX_PADS(x)					\
+	imx_iomux_v3_setup_multiple_pads(x, ARRAY_SIZE(x))
+#else
+#define IOMUX_PADS(x) MX6_##x
+#define SETUP_IOMUX_PAD(def)					\
+	imx_iomux_v3_setup_pad(MX6_##def);
 #define SETUP_IOMUX_PADS(x)					\
 	imx_iomux_v3_setup_multiple_pads(x, ARRAY_SIZE(x))
 #endif
