902ab825677d6c930f129ba36da80e5ca3d4750a
Hartmut Keller <keller@fs-net.de>
2015-11-27 01:00:22 +0100

    In fsl_esdhc.c, invalidate dcache twice to be sure
    The cache must be invalidated before the DMA for read starts. Otherwise
    if the cache mode is write-back and the data cache in this region is
    dirty, cache lines may be written back to RAM after the DMA has
    already stored its data there and before the cache is invalidated
    after the DMA has finished. In this case some data from DMA would be
    lost. Just to be sure, invalidate cache twice, once before and once
    after the DMA cycle.

--- drivers/mmc/fsl_esdhc.c
+++ drivers/mmc/fsl_esdhc.c
@@ -300,6 +300,9 @@ esdhc_send_cmd(struct mmc *mmc, struct mmc_cmd *cmd, struct mmc_data *data)
 		err = esdhc_setup_data(mmc, data);
 		if(err)
 			return err;
+
+		if (data->flags & MMC_DATA_READ)
+			check_and_invalidate_dcache_range(cmd, data);
 	}
 
 	/* Figure out the transfer arguments */
@@ -386,6 +389,11 @@ esdhc_send_cmd(struct mmc *mmc, struct mmc_cmd *cmd, struct mmc_data *data)
 			}
 		} while ((irqstat & DATA_COMPLETE) != DATA_COMPLETE);
 
+		/*
+		 * Need invalidate the dcache here again to avoid any
+		 * cache-fill during the DMA operations such as the
+		 * speculative pre-fetching etc.
+		 */
 		if (data->flags & MMC_DATA_READ)
 			check_and_invalidate_dcache_range(cmd, data);
 #endif
@@ -492,7 +500,7 @@ static int esdhc_init(struct mmc *mmc)
 	/* Set the initial clock speed */
 	mmc_set_clock(mmc, 400000);
 
-	/* MX6SX has irqstaten cleared, so enable all valid bits */
+	/* MX6SX/MX6UL have irqstaten cleared, so enable all valid bits */
 	esdhc_write32(&regs->irqstaten, 0xFFFFFFFF);
 
 	/* Disable the BRR and BWR bits in IRQSTAT */
