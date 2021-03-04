41f1f1b4f283c4b389a0e3cf4f248821701efc14
Patrick Jakob <jakob@fs-net.de>
2021-03-04 13:39:53 +0100

    Add support for I2C_GPIO to configure switch on fsimx8mm
    Due to hardware changes we have to setup some registers of the switch in
    U-Boot. Therefore we need I2C_GPIO support in U-Boot. The driver does
    not build because it fails in SPL build because some functions are not
    accessable.
    
    - Remove compilation from SPL build (Makefile.uncmd_spl)
    - Add I2C_GPIO support in device-tree and in defconfig
    - Setup switch register in fsimx8mm.c

--- board/F+S/fsimx8mm/fsimx8mm.c
+++ board/F+S/fsimx8mm/fsimx8mm.c
@@ -237,6 +237,7 @@ int checkboard(void)
 /* ---- Stage 'r': RAM valid, U-Boot relocated, variables can be used ------ */
 static int setup_fec(void);
 void fs_ethaddr_init(void);
+static int board_setup_ksz9893r(void);
 
 int board_init(void)
 {
@@ -248,6 +249,10 @@ int board_init(void)
 #ifdef CONFIG_FEC_MXC
 	setup_fec();
 #endif
+
+	if (board_type == BT_PICOCOREMX8MX) {
+		board_setup_ksz9893r();
+	}
 
 	return 0;
 }
@@ -1083,6 +1088,91 @@ static int setup_fec(void)
 			 IOMUXC_GPR_GPR1_GPR_ENET1_TX_CLK_SEL_SHIFT,
 			 0);
 	return set_clk_enet (ENET_125MHZ);
+}
+
+#define KSZ9893R_SLAVE_ADDR		0x5F
+#define KSZ9893R_CHIP_ID_MSB	0x1
+#define KSZ9893R_CHIP_ID_LSB	0x2
+#define KSZ9893R_CHIP_ID		0x9893
+#define KSZ9893R_REG_PORT_3_CTRL_1	0x3301
+#define KSZ9893R_XMII_MODES		BIT(2)
+static int ksz9893r_check_id(struct udevice *ksz9893_dev)
+{
+	uint8_t val = 0;
+	uint16_t chip_id = 0;
+	int ret;
+
+	ret = dm_i2c_read(ksz9893_dev, KSZ9893R_CHIP_ID_MSB, &val, sizeof(val));
+	if (ret != 0) {
+		printf("%s: Can´t access ksz9893r %d\n", __func__, ret);
+		return ret;
+	}
+	chip_id |= val << 8;
+	ret = dm_i2c_read(ksz9893_dev, KSZ9893R_CHIP_ID_LSB, &val, sizeof(val));
+	if (ret != 0) {
+		printf("%s: Can´t access ksz9893r %d\n", __func__, ret);
+		return ret;
+	}
+	chip_id |= val;
+
+	if (KSZ9893R_CHIP_ID == chip_id) {
+		return 0;
+	} else {
+		printf("%s: Device with ID register %x is not a ksz9893r\n", __func__,
+			   chip_id);
+		return 1;
+	}
+}
+
+static int board_setup_ksz9893r(void)
+{
+	struct udevice *bus = 0;
+	struct udevice *ksz9893_dev = NULL;
+	int ret;
+	int i2c_bus = 4;
+	uint8_t val = 0;
+
+	ret = uclass_get_device_by_seq(UCLASS_I2C, i2c_bus, &bus);
+	if (ret)
+	{
+		printf("%s: No bus %d\n", __func__, i2c_bus);
+		return -EINVAL;
+	}
+
+	ret = dm_i2c_probe(bus, KSZ9893R_SLAVE_ADDR, 0, &ksz9893_dev);
+	if (ret)
+	{
+		printf("%s: Can't find device id=0x%x, on bus %d, ret %d\n", __func__,
+			KSZ9893R_SLAVE_ADDR, i2c_bus, ret);
+		return -ENODEV;
+	}
+
+	/* offset - 16-bit address */
+	i2c_set_chip_offset_len(ksz9893_dev, 2);
+
+	/* check id if ksz9893 is available */
+	ret = ksz9893r_check_id(ksz9893_dev);
+	if (ret != 0)
+		return ret;
+
+	/* setup N301 register deaktivate In-Band Status */
+	ret = dm_i2c_read(ksz9893_dev, KSZ9893R_REG_PORT_3_CTRL_1, &val,
+					  sizeof(val));
+	if (ret != 0) {
+		printf("%s: Can´t access register %x of ksz9893r %d\n", __func__,
+			   KSZ9893R_REG_PORT_3_CTRL_1, ret);
+		return ret;
+	}
+	val &= ~KSZ9893R_XMII_MODES;
+	ret = dm_i2c_write(ksz9893_dev, KSZ9893R_REG_PORT_3_CTRL_1, &val,
+					   sizeof(val));
+	if (ret != 0) {
+		printf("%s: Can´t access register %x of ksz9893r %d\n", __func__,
+			   KSZ9893R_REG_PORT_3_CTRL_1, ret);
+		return ret;
+	}
+
+	return ret;
 }
 
 int board_phy_config(struct phy_device *phydev)
