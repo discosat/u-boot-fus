d2610f3134286d2a8516af49e9d50df6f7277217
Patrick Jakob <jakob@fs-net.de>
2021-04-06 15:53:29 +0200

    Improve fsimx8mm KSZ9893 Switch setup
    For revision 1.10 of PCB PicoCoreMX8MX there are two strapping pins
    which setup the Serial Interface Mode (MDIO, I2C, SPI). There is a HW
    bug in revision 1.10 so we are in the wrong Serial Interface Mode and
    are unable to communicate via I2C. Due to this issue we are
    pre-configuring the strapping pin as GPIO and setup the correct value.
    Afterwards we resetting the chip so the chip can parse the new strapping
    configuration and we are able to communicate via I2C with the chip.
    Later on the device tree will setup the correct mux option for the
    strapping pin.
    
    The setup_iomux_fec resets the PHY in board file but we only need this
    for PCB PicoCoreMX8MX, because we have to setup the correct strapping
    option before the network infrastructure will be loaded. For PCB
    PicoCoreMX8MM the reset is done by the network infrastructure and is
    setup by the device tree.

--- board/F+S/fsimx8mm/fsimx8mm.c
+++ board/F+S/fsimx8mm/fsimx8mm.c
@@ -1034,8 +1034,10 @@ int board_late_init(void)
 
 #ifdef CONFIG_FEC_MXC
 #define FEC_RST_PAD IMX_GPIO_NR(1, 5)
+#define FEC_SIM_PAD IMX_GPIO_NR(1, 26)
 static iomux_v3_cfg_t const fec1_rst_pads[] = {
 	IMX8MM_PAD_GPIO1_IO05_GPIO1_IO5 | MUX_PAD_CTRL(NO_PAD_CTRL),
+	IMX8MM_PAD_ENET_RD0_GPIO1_IO26 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
 static void setup_iomux_fec(void)
@@ -1043,6 +1045,16 @@ static void setup_iomux_fec(void)
 	imx_iomux_v3_setup_multiple_pads(fec1_rst_pads,
 					 ARRAY_SIZE (fec1_rst_pads));
 
+	/* before resetting the ethernet switch for PCoreMX8MX revision 1.10 we
+	 * have to configure a strapping pin to use the Serial Interface Mode
+	 * "I2C". The eth node in device tree will overwrite the mux option for
+	 * ENET_RD0 so we don´t have to change it back to dedicated function.
+	 */
+	if(fs_board_get_rev() == 110) {
+		gpio_request(FEC_SIM_PAD, "SerialInterfaceMode");
+		gpio_direction_output(FEC_SIM_PAD, 1);
+		gpio_free(FEC_SIM_PAD);
+	}
 	gpio_request(FEC_RST_PAD, "fec1_rst");
 	fs_board_issue_reset(11000, 1000, FEC_RST_PAD, ~0, ~0);
 }
@@ -1065,7 +1077,8 @@ static int setup_fec(void)
 	struct iomuxc_gpr_base_regs *const iomuxc_gpr_regs =
 		(struct iomuxc_gpr_base_regs*) IOMUXC_GPR_BASE_ADDR;
 
-	setup_iomux_fec();
+	if(fs_board_get_type() == BT_PICOCOREMX8MX)
+		setup_iomux_fec();
 
 	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
 	clrsetbits_le32(&iomuxc_gpr_regs->gpr[1],
