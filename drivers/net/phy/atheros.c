4cb3806356e62584717a38e6a444555b19fd8280
Kay Mueller <mueller@fs-net.de>
2022-07-21 11:37:38 +0200

    Enable CLK_25M output clock selection for AR8035

--- drivers/net/phy/atheros.c
+++ drivers/net/phy/atheros.c
@@ -53,15 +53,54 @@ static int ar8031_config(struct phy_device *phydev)
 	return 0;
 }
 
+#define AR8035_CLK_25M_BIT_SHIFT	3
+#define AR8035_CLK_25M_25MHZ_XTAL	0x0
+#define AR8035_CLK_25M_50MHZ_PLL	0x1
+#define AR8035_CLK_25M_62_5MHZ_PLL	0x2
+#define AR8035_CLK_25M_125MHZ_PLL	0x3
+
 static int ar8035_config(struct phy_device *phydev)
 {
 	int regval;
+	u16 clk_25m_reg = 0;
+	ofnode node;
+	struct ofnode_phandle_args phandle_args;
+	u32 freq;
+
+	/* KM 2022-07-21:
+	 * For now, the phydev is associated with the ethernet device,
+	 * so we need to get the phy through phandle.
+	 * This can change in later versions of the UBoot */
+	node = phy_get_ofnode(phydev);
+
+	if (ofnode_valid(node)) {
+		ofnode_parse_phandle_with_args(node, "phy-handle", NULL, 0, 0, &phandle_args);
+		freq = ofnode_read_u32_default(phandle_args.node, "qca,clk-out-frequency", 0);
+		switch (freq) {
+		case 25000000:
+			clk_25m_reg = AR8035_CLK_25M_25MHZ_XTAL;
+			break;
+		case 50000000:
+			clk_25m_reg = AR8035_CLK_25M_50MHZ_PLL;
+			break;
+		case 62500000:
+			clk_25m_reg = AR8035_CLK_25M_62_5MHZ_PLL;
+			break;
+		case 125000000:
+			clk_25m_reg = AR8035_CLK_25M_125MHZ_PLL;
+			break;
+		default:
+			clk_25m_reg = AR8035_CLK_25M_25MHZ_XTAL;
+			break;
+		}
+		clk_25m_reg = clk_25m_reg << AR8035_CLK_25M_BIT_SHIFT;
+	}
 
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xd, 0x0007);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xe, 0x8016);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xd, 0x4007);
 	regval = phy_read(phydev, MDIO_DEVAD_NONE, 0xe);
-	phy_write(phydev, MDIO_DEVAD_NONE, 0xe, (regval|0x0018));
+	phy_write(phydev, MDIO_DEVAD_NONE, 0xe, (regval|clk_25m_reg));
 
 	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x05);
 	regval = phy_read(phydev, MDIO_DEVAD_NONE, 0x1e);
