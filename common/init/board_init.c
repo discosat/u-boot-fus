6d77f3d7bb02d03aa71cc4d2c2ec4bbe47ac27c2
Hartmut Keller <keller@fs-net.de>
2022-07-25 15:51:25 +0200

    Do not add MALLOC_F to stack region if CONFIG_MALLOC_F_ADDR is set
    Setting CONFIG_MALLOC_F_ADDR shows the intent, that the address for the
    MALLOC_F area should be set specifically to this address, not be computed
    automatically and added to some other region (here: stack). So skip the
    automatic computation in this case.

--- common/init/board_init.c
+++ common/init/board_init.c
@@ -45,7 +45,7 @@ __weak void arch_setup_gd(struct global_data *gd_ptr)
 ulong board_init_f_alloc_reserve(ulong top)
 {
 	/* Reserve early malloc arena */
-#if CONFIG_VAL(SYS_MALLOC_F_LEN)
+#if CONFIG_VAL(SYS_MALLOC_F_LEN) && !defined(CONFIG_MALLOC_F_ADDR)
 	top -= CONFIG_VAL(SYS_MALLOC_F_LEN);
 #endif
 	/* LAST : reserve GD (rounded up to a multiple of 16 bytes) */
@@ -120,7 +120,7 @@ void board_init_f_init_reserve(ulong base)
 	 * Use gd as it is now properly set for all architectures.
 	 */
 
-#if CONFIG_VAL(SYS_MALLOC_F_LEN)
+#if CONFIG_VAL(SYS_MALLOC_F_LEN) && !defined(CONFIG_MALLOC_F_ADDR)
 	/* go down one 'early malloc arena' */
 	gd->malloc_base = base;
 	/* next alloc will be higher by one 'early malloc arena' size */
