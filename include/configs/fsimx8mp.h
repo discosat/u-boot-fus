769fdf47c983977928650a93d41622db01f886a7
Anatol Derksen <derksen@fs-net.de>
2022-11-08 13:52:23 +0100

    Improve primary eth interface for fsimx8mp
    Improve primary eth interface for fsimx8mp.
    Change CONFIG_ETHPRIME to eth0 because of
    new aliases in device tree.

--- include/configs/fsimx8mp.h
+++ include/configs/fsimx8mp.h
@@ -243,7 +243,7 @@
 #define CONFIG_SYS_DISCOVER_PHY
 
 #if defined(CONFIG_CMD_NET)
-#define CONFIG_ETHPRIME                 "eth1" /* Set qos to primary since we use its MDIO */
+#define CONFIG_ETHPRIME                 "eth0" /* Set qos to primary since we use its MDIO */
 #define FDT_SEQ_MACADDR_FROM_ENV
 
 #define CONFIG_FEC_XCV_TYPE             RGMII
