272e84ea509f8e3d2730b00a22856508ed6a7015
Hartmut Keller <keller@fs-net.de>
2022-12-02 18:25:33 +0100

    Fix SDP timeout errors on fsimx8x
    When NBoot download is finished (OUT report 2), the board will answer
    with an IN report 3 (HAB status). Then it sends an IN report 4 (transmit
    status) right after. This transmission was not acknowledged correctly by
    the UUU tool on the host side. Research showed, that UUU always expects
    65 bytes when loading these reports. But report 3 only sends 5 bytes.
    Unfortunately, older Windows USB drivers (e.g. on Win 7) will not detect
    this correctly and take bytes from the next report to fill up the first
    report. This causes the second report to be incomplete and UUU does not
    see the second report, causing a timeout error.
    
    By setting both reports, 3 and 4 to the length of 65 bytes (1 byte report
    ID, 64 bytes data), the consolidation of the messages on the Windows side
    can be avoided and UUU sees all reports correctly.

--- drivers/usb/gadget/f_sdp.c
+++ drivers/usb/gadget/f_sdp.c
@@ -208,13 +208,31 @@ static struct hid_report sdp_hid_report = {
 		0x95, 0x40, /* Report Count */
 		0x91, 0x02, /* Output Data */
 
+		/*
+		 * When sending a file with the write -f command (OUT report
+		 * 2), the answer is IN report 3 (HAB status) immediately
+		 * followed by IN report 4 (transmit status). However UUU on
+		 * the host side reads all IN reports with 65 bytes length (1
+		 * byte report ID plus 64 bytes data). If the first report has
+		 * less than 65 bytes, older USB drivers on Windows may
+		 * consolidate the two reports in one single message and UUU
+		 * fails to see the second report. So either have a rather
+		 * long delay after the first report to make sure that it is
+		 * fully handled on the host side before the second report
+		 * comes in or (as we do it here) make sure that both reports
+		 * use the full 65 bytes so that no free space is available in
+		 * the first report anymore, which successfully avoids the
+		 * message consolidation. This seems to work well on all
+		 * platforms.
+		 */
 		0x85, 0x03, /* Report ID */
 		0x19, 0x01, /* Usage Minimum */
 		0x29, 0x01, /* Usage Maximum */
 		0x15, 0x00, /* Local Minimum */
 		0x26, 0xFF, 0x00, /* Local Maximum? */
 		0x75, 0x08, /* Report Size 8 */
-		0x95, 0x04, /* Report Count */
+		//### 0x95, 0x04, /* Report Count */
+		0x95, 0x40, /* Report Count */
 		0x81, 0x02, /* Input Data */
 
 		0x85, 0x04, /* Report ID */
@@ -734,7 +752,8 @@ static void sdp_handle_in_ep(void)
 
 		status = SDP_SECURITY_OPEN;
 		memcpy(&data[1], &status, 4);
-		sdp_func->in_req->length = 5;
+		//### sdp_func->in_req->length = 5;
+		sdp_func->in_req->length = 65;
 		usb_ep_queue(sdp_func->in_ep, sdp_func->in_req, 0);
 		sdp_func->state = SDP_STATE_TX_SEC_CONF_BUSY;
 		break;
