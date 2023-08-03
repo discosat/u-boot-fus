b3d58ffaf473280425a0b0b09c434b211ec56fa1
Hartmut Keller <keller@fs-net.de>
2023-08-03 20:19:10 +0200

    Actually skip full bad blocks in nand_util.c
    When writing to a page that is not at the beginning of a block (or even
    at the beginning of an MTD partition), it means the caller wants to
    continue writing to an already partly written image. So it is essential
    that the page offset is kept if the first block is bad. So if writing
    to page n in block x, and block x is bad, skip block x and write to page
    n in block x+1. The previous code wrote to page 0 of block x+1, which
    does not make sense.
    
    The same is true when reading data.
    
    So change functions check_skip_len(), nand_write_skip_bad() and
    nand_read_skip_bad() accordingly.
    
    If reading or writing starts with the first page of a block, for
    example when writing to a partition, the result is the same as before,
    so in typical cases, this should make no difference. But we need this
    new behaviour when reading and writing images with fsimage load/save.
    
    Also fix an indentation error in nand_base.c.

--- drivers/mtd/nand/raw/nand_util.c
+++ drivers/mtd/nand/raw/nand_util.c
@@ -435,8 +435,10 @@ static int check_skip_len(struct mtd_info *mtd, loff_t offset, size_t length,
 
 		if (!nand_block_isbad(mtd, block_start))
 			len_excl_bad += block_len;
-		else
+		else {
+			block_len = mtd->erasesize;
 			ret = 1;
+		}
 
 		offset += block_len;
 		*used += block_len;
@@ -649,7 +651,7 @@ int nand_write_skip_bad(struct mtd_info *mtd, loff_t offset, size_t *length,
 
 		if (nand_block_isbad(mtd, block_start)) {
 			printf("Skip bad block 0x%08llx\n", block_start);
-			offset += mtd->erasesize - block_offset;
+			offset += mtd->erasesize;
 			continue;
 		}
 
@@ -788,7 +790,7 @@ int nand_read_skip_bad(struct mtd_info *mtd, loff_t offset, size_t *length,
 		    && nand_block_isbad(mtd, offset & ~(mtd->erasesize - 1))) {
 			printf("Skipping bad block at 0x%08llx\n",
 				offset & ~(mtd->erasesize - 1));
-			offset += mtd->erasesize - block_offset;
+			offset += mtd->erasesize;
 			continue;
 		}
 
@@ -879,8 +881,7 @@ int nand_convert_skip_bad(struct mtd_info *mtd, loff_t offset, size_t *length,
 
 		WATCHDOG_RESET();
 
-		if (need_skip
-		    && nand_block_isbad(mtd, offset)) {
+		if (need_skip && nand_block_isbad(mtd, offset)) {
 			printf("Skipping bad block at 0x%08llx\n", offset);
 			offset += erasesize;
 			continue;
